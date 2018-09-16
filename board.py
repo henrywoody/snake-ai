736862a1fc6f3d6b78784a4fe9f155ef90a74be9
henrywoody <github@henrywoody.com>
2018-09-15 21:43:08 -0700

    updates Board class for running everything

--- board.py
+++ board.py
@@ -1,45 +1,97 @@
+import sys
+import numpy as np
+import random
 import pygame
 from pygame.locals import *
 
 from snake import Snake
+from food import Food
+import utils
 
 
 class Board:
-	def __init__(self, width, height):
+	def __init__(self, width, height, snake_genome=[0,0], num_food=1, seed=2188357, animation_on=True):
 		self.width = width
 		self.height = height
+		self.animation_on = animation_on
 		self.color = (34, 139, 34)
 
-		init_snake_position = [self.width/2, self.height/2]
-		init_snake_direction = 1.5
-		self.snake = Snake(init_snake_position, init_snake_direction)
-		self.foods = []
+		random.seed(seed)
 
-		pygame.init()
-		self.fps_clock = pygame.time.Clock()
-		self.fps = 60
-		self.surface = pygame.display.set_mode((self.width, self.height))
-		pygame.display.set_caption('Snake')
+		self.snake = self.spawn_snake(genome=snake_genome)
 
-	def run(self):
+		self.num_food = num_food
+		self.foods = self.spawn_food(num_food)
+
+		if self.animation_on:
+			pygame.init()
+			self.fps_clock = pygame.time.Clock()
+			self.fps = 60
+			self.surface = pygame.display.set_mode((self.width, self.height))
+			pygame.display.set_caption('Snake')
+
+	def run(self, max_time=np.inf):
+		timer = 0
 		while self.snake.is_alive:
 			self.update()
-			self.draw()
 
-			self.check_quit()
+			if self.animation_on:
+				self.update_animation()
+			
+			timer += 1
+			if timer > max_time:
+				break
 
-			self.fps_clock.tick(self.fps)
+		return len(self.snake.body), self.snake.genome
 
 	def update(self):
-		self.snake.move()
+		self.snake.update(self.foods)
+
+		next_foods = []
+		for food in self.foods:
+			if utils.are_touching(self.snake.body[0], food):
+				self.snake.grow()
+			else:
+				next_foods.append(food)
+		self.foods = next_foods
+		if len(self.foods) != self.num_food:
+			self.foods += self.spawn_food(self.num_food - len(self.foods))
+
+	def spawn_snake(self, genome):
+		init_snake_position = [self.width/2, self.height/2]
+		init_snake_direction = 0
+		return Snake(init_snake_position, init_snake_direction, genome=genome)
+
+	def spawn_food(self, num):
+		return [Food(self.get_random_position()) for _ in range(num)]
+
+	def update_animation(self):
+		self.draw()
+		self.check_quit()
+		self.fps_clock.tick(self.fps)
 
 	def draw(self):
 		self.surface.fill(self.color)
+		
 		self.snake.draw(self.surface)
+		for food in self.foods:
+			food.draw(self.surface)
+
 		pygame.display.update()
 
+	def get_random_position(self):
+		x = random.random() * self.width
+		y = random.random() * self.height
+		return x,y
+
 	def check_quit(self):
-		for event in pygame.event.get():
-			if event == QUIT:
-				pygame.quit()
-				sys.exit()
+		for event in pygame.event.get(QUIT):
+			self.terminate()
+		for event in pygame.event.get(KEYUP):
+			if event.key == K_ESCAPE:
+				self.terminate()
+			pygame.event.post(event)
+
+	def terminate(self):
+		pygame.quit()
+		sys.exit()
